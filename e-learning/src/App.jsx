f885a2abc9378acc8991aafe98fb6b435a32dc43
Aires23Matos <aires2matos18@gmail.com>
2025-03-07 22:09:35 +0100

    creating the service file in components/service

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-import Navbar from "./components/Navbar/Navbar"
-import Hero from "./components/Hero/Hero"
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/service/Service";
+
 function App() {
   return (
     <main className='overflow-x-hidden bg-white text-dark'>
       <Hero/>
+      <Services/>
     </main>
   )
 }
