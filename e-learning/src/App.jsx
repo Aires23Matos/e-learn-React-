0fe42c04ae3200b745937be4b7d2a76d09a93d5b
Aires23Matos <aires2matos18@gmail.com>
2025-03-08 00:55:11 +0100

    creating the Banner file in components

--- e-learning/src/App.jsx
+++ e-learning/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
 import Services from "./components/service/Service";
+import Banner from "./components/Banner/Banner";
 
 function App() {
   return (
     <main className='overflow-x-hidden bg-white text-dark'>
       <Hero/>
       <Services/>
+      <Banner/>
     </main>
   )
 }
